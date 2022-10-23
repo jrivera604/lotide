4d476164d6c5e1db49fd0319d05fc9c032cee80f
jrivera604 <joshrivera91@hotmail.com>
2022-10-23 19:59:59 +0000

    my head function

---
+++ head.js
@@ -0,0 +1,22 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function(actual, expected) {
+  let head = actual[0]
+  if (head === expected) {
+    console.log(`✅ Assertion Passed: ${head} === ${expected}`);
+  }
+  if (head !== expected) {
+    console.log(`❎ Assertion Failed: ${head} !== ${expected}`);
+  }
+};
+
+
+
+
+// TEST CODE
+assertEqual(["Lighthouse", "Labs", "Bootcamp"], "Lighthouse");
+assertEqual(['hello', 'hello'], 'Hi');
+assertEqual([1, 1], 1);
+assertEqual([1, 2], 2);
+assertEqual([],1)
+assertEqual([5], 5)
