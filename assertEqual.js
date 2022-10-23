5c1b8f01006894ec8ef7634ba3abfea0f25b7b63
jrivera604 <joshrivera91@hotmail.com>
2022-10-23 00:12:17 +0000

    code for assertion

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅ Assertion Passed: [actual] === [expected]');
+  }
+  if (actual !== expected) {
+    console.log('❎ Assertion Failed: [actual] !== [expected]');
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual('hello', 'hello');
+assertEqual(1, 1);
+assertEqual(1, 2);
