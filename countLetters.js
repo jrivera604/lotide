ae32cdf8eecd4260c65fbc16e130050b255d7c38
jrivera604 <joshrivera91@hotmail.com>
2022-10-29 00:05:09 +0000

    commit countLetters

---
+++ countLetters.js
@@ -0,0 +1,38 @@
+const countLetters = function(phrase) {
+  let result = [];
+  for (const letter of phrase) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+    
+  }
+
+  return result;
+
+};
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`❎ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const test = countLetters ("lighthouse labs")
+console.log (test["l"])
+assertEqual(test["l"], 2 )
+assertEqual(test["i"], 1)
+assertEqual(test["x"], 1)
+
+
+
+
+
+
