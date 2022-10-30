b4dff25a096dda18ef20155fcb41a8b8da781ada
jrivera604 <joshrivera91@hotmail.com>
2022-10-30 20:15:37 +0000

    export code

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const findKeyByValue = function(object, value) {
+  
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`❎ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
