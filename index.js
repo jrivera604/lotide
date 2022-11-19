ce74c914b453690e682685618af51add98a02101
jrivera604 <joshrivera91@hotmail.com>
2022-11-19 03:29:49 +0000

    addede shorthand syntax for index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // index.js
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
+const head   = require('./head.js');
+const tail   = require('./tail.js');
+const middle = require('./middle.js');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  head,
+  tail,
+  middle
 };
